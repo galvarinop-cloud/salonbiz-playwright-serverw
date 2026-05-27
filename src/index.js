6aa17b979b6a051733acfafe86c50122c8a4f703
galvarinop-cloud <galvarinop@icloud.com>
2026-05-27 11:13:35 -0400

    Fix: syntax error in PHONE_BOOKABLE_SERVICES line 23 - remove stray h

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ let cookieStateSetAt = 0;
 const COOKIE_TTL_MS = Number(process.env.COOKIE_TTL_MS || 1000 * 60 * 60 * 6);
 
 const PHONE_BOOKABLE_SERVICES_RAW = process.env.PHONE_BOOKABLE_SERVICES || "";
-const PHONE_BOOKABLE_SERVICES = new Set(h
+const PHONE_BOOKABLE_SERVICES = new Set(
   PHONE_BOOKABLE_SERVICES_RAW.split(/\r?\n|,/).map((s) => s.trim()).filter(Boolean)
 );
 
